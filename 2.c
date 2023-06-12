7c780062a153e70a0da0b395f5fcbc083a411f39
Lyriom <130936689+Lyriom@users.noreply.github.com>
2023-06-12 15:02:46 -0500

    2 version

--- 2.c
+++ 2.c
@@ -1,8 +1,11 @@
-in int main(int argc, char const *argv[])
-{
-    /* code */
-    printf(holaaa);
-    printf(holaaa);
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello world");
+    printf("holaaaaaaaaaaaaaa");
+
     return 0;
     
 }
