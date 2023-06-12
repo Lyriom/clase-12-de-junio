70ed4d646f3256742cc9f9dd1b06f28d51d7abdb
Lyriom <130936689+Lyriom@users.noreply.github.com>
2023-06-12 15:21:07 -0500

    5 version

--- 1er_ejercio.c
+++ 1er_ejercio.c
@@ -1,7 +1,18 @@
-in int main(int argc, char const *argv[])
-{
-    
-    printf(holaaa);
+#include<stdio.h>
+int main(){
+
+
+    /*Hasta el momento se asignaba directamente un valor que se queria almacenar en una variable, pero tambien existe la posibilidad de
+     asignar la direccion de memoria de un determinado valor a una variable o a una funcion, asi se evitara colocar o repetir una copia de un valor cada
+     vez que se requiere este dato.*/
+     
+    char *m, varChar;
+    m = &varChar;
+    *m='B';
+    int n = 678;
+    float p = 3.1416;
+    printf("\nEl char es: %c %d", m, varChar);
+    printf("\nEl entero es: %d", n);
+    printf("\nEl float es: %f", p);
     return 0;
-    
 }
